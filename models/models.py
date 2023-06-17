0cadf166db62ff4ff8b3202b759d1e5158fe05b0
Laurence Liang <laurence.w.liang@gmail.com>
2023-06-17 11:16:02 -0700

    Comment unused fields

--- models/models.py
+++ models/models.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel
 
 class UserResp(BaseModel):
     name:       str                         # child's name
-    grade:      str | None = None           # child's current grade
-    school:     str | None = None           # child's current school
+    # grade:      str | None = None           # child's current grade
+    # school:     str | None = None           # child's current school
     interests:  list[str] = []              # list of interests (in string)
 
-    curriculum: str | None = None           # current curriculum (i.e. brain)
+    # curriculum: str | None = None           # current curriculum (i.e. brain)
     topic:      str                         # question about main topic
 
     modifier:   str                         # describe the tone (i.e. please make it mysterious)

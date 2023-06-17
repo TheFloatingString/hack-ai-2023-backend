d48cf5d9823639790924a55a4c5cfb186bc7ce5b
Laurence Liang <laurence.w.liang@gmail.com>
2023-06-16 18:59:47 -0700

    WIP add test Open AI API call

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 from fastapi import FastAPI
+import openai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+resp = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="What is the largest city in the United States?",
+        max_tokens=200,
+        temperature=0
+        )
+
+print(resp)
 
 app = FastAPI()
 

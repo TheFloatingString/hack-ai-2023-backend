0b00952feee4625d74b279aac3d5572cc209a8e6
Laurence Liang <laurence.w.liang@gmail.com>
2023-06-17 00:18:17 -0700

    Add s2 and s3 string generation

--- src/external_api_wrapper.py
+++ src/external_api_wrapper.py
@@ -18,6 +18,8 @@ class ExternalWrapper:
         self.character_descriptors = character_descriptors
         self.openai_obj = openai_obj
 
+        self.S1_PROMPT = ""
+
     def return_response(self):
 
         return_dict = {
@@ -53,13 +55,20 @@ class ExternalWrapper:
 
         return_dict["data"]["s1"]["highlightedText"] = re.findall("<b>(.*?)</b>", s1_raw_text)
 
+
         return_dict["data"]["question"]["text"] = self.generate_question()
 
+        return_dict["data"]["s2"]["text"] = self.generate_text_s2(
+            return_dict["data"]["question"]["text"],
+            return_dict["data"]["s1"]["text"]
+            )
+
+        return_dict["data"]["s3"]["text"] = self.generate_text_s3(return_dict["data"]["s1"]["text"])
         return return_dict
 
 
     def generate_text_s1(self):
-        S1_PROMPT = f"""
+        self.S1_PROMPT = f"""
             Write a story that teaches a child about {self.topic}. 
             The story is centered around a character called {self.name} with the following characteristics: {", ".join(self.character_descriptors)}. 
             The story is set in the universe of {self.character_environment}. 
@@ -69,12 +78,39 @@ class ExternalWrapper:
 
         resp = self.openai_obj.Completion.create(
             model="text-davinci-003",
-            prompt=S1_PROMPT,
+            prompt=self.S1_PROMPT,
             max_tokens=350,
             temperature=0
             )
         
         return resp["choices"][0]["text"]
+
+
+    def generate_text_s2(self, question, s1_generated_text):
+        
+        completion = self.openai_obj.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": self.S1_PROMPT},
+                {"role": "system", "content": s1_generated_text},
+                {"role": "user", "content": f"{self.name} incorrectly answered the question: {question}. Write a brief paragraph using the previous story to explain why the {self.name} answered the question incorrectly. Do not reference the user directly in the response."}
+            ]
+        )
+
+        return completion["choices"][0]["message"]["content"]
+
+    def generate_text_s3(self, s1_generated_text):
+        
+        completion = self.openai_obj.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "user", "content": self.S1_PROMPT},
+                {"role": "system", "content": s1_generated_text},
+                {"role": "user", "content": "Generate a kind ending to this story."}
+            ]
+        )
+
+        return completion["choices"][0]["message"]["content"]
 
     def generate_question(self):
         PROMPT = f"""
